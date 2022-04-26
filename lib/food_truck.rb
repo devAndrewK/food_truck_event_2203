bdf4cf061f403d1c3529548c75d7f20b1063c615
Andrew King <dev.andrewking@gmail.com>
2022-04-26 10:29:24 -0500

    add stock and check_stock methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -6,4 +6,21 @@ def initialize(name)
   @inventory = {}
 end
 
+def stock(item, quantity)
+  if @inventory[item].nil?
+    @inventory[item] = quantity
+  else
+    @inventory[item] += quantity
+  end
+end
+
+def check_stock(item)
+    if @inventory[item].nil?
+     return 0
+   else
+    @inventory[item]
+  end
+end
+
+
 end
