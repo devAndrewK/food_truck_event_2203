7f1b8235e4431e07a25accff814b6d7dbab2f42e
Andrew King <dev.andrewking@gmail.com>
2022-04-26 10:16:55 -0500

    add food truck exist and attr tests

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,18 @@
+require './lib/food_truck'
+RSpec.describe FoodTruck do
+  before :each do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  it 'exists' do
+    expect(@food_truck).to be_an(FoodTruck)
+  end
+
+  it 'has attributes' do
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
+    expect(@food_truck.inventory).to eq({})
+  end
+
+end
